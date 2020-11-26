b75beec408d18e6dd9b71b59de3a6e2ebadfdd27
Andrew Stockdale <andrew.stockdale@eigentech.com>
2020-11-26 15:43:27 +0000

    Add tests and superuser creation

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -20,3 +20,17 @@ class ModelTests(TestCase):
       user = get_user_model().objects.create_user(email,'test123')
 
       self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+      with self.assertRaises(ValueError):
+        get_user_model().objects.create_user(None, 'password')
+    
+    def test_create_superuser(self):
+      """Test creating a new superuser"""
+      user = get_user_model().objects.create_superuser(
+        'test@gmail.com',
+        'test123'
+      )
+
+      self.assertTrue(user.is_superuser)
+      self.assertTrue(user.is_staff)
